e148d01b54e37eb6f533fa0df62bbfc7d3d8a25e
Jan <jnk@proton.me>
2026-03-14 22:56:22 +0100

    calculate and show used pto

--- calculator.js
+++ calculator.js
@@ -2,6 +2,7 @@ dayjs.extend(window.dayjs_plugin_weekday);
 
 const apiUrl = 'https://api.clockify.me/api/v1';
 const temporalRe = /PT(\d{1,2}H)?(\d{1,2}M)?(\d{1,2}S)?/;
+const holidayProjectId = '63dcf27783d3c529999f1304';
 
 const triggerBtn = document.querySelector('#trigger-btn');
 triggerBtn.addEventListener('click', async () => {
@@ -26,6 +27,7 @@ const workingDaysInputs = [
     document.querySelector('#working-day-sun'),
 ];
 const workingHoursPerDayInput = document.querySelector('#working-hours-per-day');
+const ptoInput = document.querySelector('#pto');
 
 // apply selected preset by checking appropriate boxes and toggling visibility
 function applyPreset(preset) {
@@ -55,6 +57,7 @@ function loadSettings() {
     const endDefault = today;
     const workingHoursDefault = 7.6; // 38 hours per week
     const defaultWorkingDays = [1, 2, 3, 4, 5]; // Mon-Fri
+    const defaultPto = 30;
 
     const storedStart = localStorage.getItem('start');
     startInput.value = storedStart || startDefault.format('YYYY-MM-DD');
@@ -64,6 +67,9 @@ function loadSettings() {
 
     const storedHours = localStorage.getItem('workingHoursPerDay');
     workingHoursPerDayInput.value = storedHours !== null ? storedHours : workingHoursDefault;
+
+    const storedPto = localStorage.getItem('pto');
+    ptoInput.value = storedPto !== null ? storedPto : defaultPto;
 
     // load preset and working days
     const storedPreset = localStorage.getItem('workingDaysPreset') || 'mon-fri';
@@ -107,30 +113,47 @@ const totalWorkDaysOut = document.querySelector('#total-work-days');
 const totalTimeToWorkOut = document.querySelector('#total-time-to-work');
 const totalHoursWorkedOut = document.querySelector('#total-hours-worked');
 const overtimeOut = document.querySelector('#overtime');
+const holidayOut = document.querySelector('#holiday');
+const holidayDetailsOut = document.querySelector('#holiday-details');
 
 async function calculateOvertimeAsync() {
-  saveSettings();
+    saveSettings();
 
-  apiKey = apiKeyInput.value;
-  const start = dayjs(startInput.value);
-  const end = endInput.value ? dayjs(endInput.value) : dayjs();
-  const workingDays = workingDaysInputs.filter((d) => d.checked).map((d) => Number.parseInt(d.value));
-  const workingHoursPerDay = workingHoursPerDayInput.value;
+    apiKey = apiKeyInput.value;
+    const start = dayjs(startInput.value);
+    const end = endInput.value ? dayjs(endInput.value) : dayjs();
+    const workingDays = workingDaysInputs.filter((d) => d.checked).map((d) => Number.parseInt(d.value));
+    const workingHoursPerDay = workingHoursPerDayInput.value;
 
-  const totalWorkDays = getTotalWorkingDays(start, end, workingDays);
-  totalWorkDaysOut.innerText = totalWorkDays;
+    const totalWorkDays = getTotalWorkingDays(start, end, workingDays);
+    totalWorkDaysOut.innerText = totalWorkDays;
 
-  const totalTimeToWork = totalWorkDays * workingHoursPerDay;
-  totalTimeToWorkOut.innerText = totalTimeToWork;
+    const totalTimeToWork = totalWorkDays * workingHoursPerDay;
+    totalTimeToWorkOut.innerText = totalTimeToWork;
 
-  const { userId, workspaceId } = await getUserAndWorkspaceIdAsync();
+    const { userId, workspaceId } = await getUserAndWorkspaceIdAsync();
 
-  const clockifyTimes = await getClockifyTimesAsync(start, end, workspaceId, userId);
-  const totalHoursWorked = clockifyTimes.reduce((a, b) => a + b, 0);
-  totalHoursWorkedOut.innerText = totalHoursWorked;
+    const clockifyEntries = await getClockifyEntriesAsync(start, end, workspaceId, userId);
+    const workingHours = clockifyEntries.map((t) =>
+        t.timeInterval.duration ? calculateHours(t.timeInterval.duration) : 0,
+    );
+    const totalHoursWorked = workingHours.reduce((a, b) => a + b, 0);
+    totalHoursWorkedOut.innerText = totalHoursWorked;
 
-  const overtime = totalHoursWorked - totalTimeToWork;
-  overtimeOut.innerText = overtime;
+    const overtime = totalHoursWorked - totalTimeToWork;
+    overtimeOut.innerText = overtime;
+
+    const usedHoliday = clockifyEntries.filter((t) => t.projectId === holidayProjectId);
+    const usedHolidayHours = usedHoliday
+        .map((t) => (t.timeInterval.duration ? calculateHours(t.timeInterval.duration) : 0))
+        .reduce((a, b) => a + b, 0);
+    holidayOut.innerText = usedHolidayHours / workingHoursPerDay;
+    holidayDetailsOut.innerHTML = usedHoliday
+        .map(
+            (t) =>
+                `<li>${dayjs(t.timeInterval.start).format('DD.MM.YY')}: ${calculateHours(t.timeInterval.duration)}</li>`,
+        )
+        .join('');
 }
 
 async function getUserAndWorkspaceIdAsync() {
@@ -144,27 +167,27 @@ async function getUserAndWorkspaceIdAsync() {
   return { userId: result.id, workspaceId: result.defaultWorkspace };
 }
 
-async function getClockifyTimesAsync(start, end, workspaceId, userId) {
-  const times = [];
-  let page = 1;
-  while (true) {
-    const params = new URLSearchParams();
-    params.append('start', start.add(-1, 'day').format('YYYY-MM-DDThh:mm:ssZ'));
-    params.append('end', end.add(1, 'day').format('YYYY-MM-DDThh:mm:ssZ'));
-    params.append('page', page);
-    params.append('page-size', 200);
+async function getClockifyEntriesAsync(start, end, workspaceId, userId) {
+    const times = [];
+    let page = 1;
+    while (true) {
+        const params = new URLSearchParams();
+        params.append('start', start.add(-1, 'day').format('YYYY-MM-DDThh:mm:ssZ'));
+        params.append('end', end.add(1, 'day').format('YYYY-MM-DDThh:mm:ssZ'));
+        params.append('page', page);
+        params.append('page-size', 200);
 
-    const response = await fetch(`${apiUrl}/workspaces/${workspaceId}/user/${userId}/time-entries?${params}`, {
-      headers: { 'x-api-key': apiKey },
-    });
-    const pagedTimes = await response.json();
-    if (pagedTimes.length === 0) {
-      break;
+        const response = await fetch(`${apiUrl}/workspaces/${workspaceId}/user/${userId}/time-entries?${params}`, {
+            headers: { 'x-api-key': apiKey },
+        });
+        const pagedTimes = await response.json();
+        if (pagedTimes.length === 0) {
+            break;
+        }
+        times.push(...pagedTimes);
+        page++;
     }
-    times.push(...pagedTimes.map((t) => (t.timeInterval.duration ? calculateHours(t.timeInterval.duration) : 0)));
-    page++;
-  }
-  return times;
+    return times;
 }
 
 function calculateHours(duration) {
